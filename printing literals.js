e8846e0f364d1b6bb6ee0027d49265b94f781024
Vismayaps2004 <vismayapshaji@gmail.com>
2025-09-19 16:59:03 +0530

    Create a program for printing literals

---
+++ printing literals.js
@@ -0,0 +1,8 @@
+console.log("Vismaya");
+console.log(10000)
+console.log("Vismayaa : "+1000);
+console.log(10000,150000,2000000)
+console.log(10000 +","+15000)
+console.log("A","a","B","b","C","c","D","d")
+console.count("viss")
+console.count("mayaa")
