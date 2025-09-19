cfcb288490176ecb5540c3e6f893be3dc2e75d3a
Vismayaps2004 <vismayapshaji@gmail.com>
2025-09-19 16:44:37 +0530

    Create a file for printing series start with 2 and print upto 10 numbers with common difference

---
+++ arithrmatic_progression/ap.js
@@ -0,0 +1,7 @@
+let firstNumber = 2;
+const noOfTerms = 10;
+for (let count = 1; count <= noOfTerms; count++) {
+    console.log(firstNumber);
+    const commonDifference = 3;
+    firstNumber = firstNumber + commonDifference;
+}
