b7680ef4eec147a9cb5a8873d3edece55d8cadd1
Vismayaps2004 <vismayapshaji@gmail.com>
2025-09-19 16:57:34 +0530

    Create a proggram to convert quotient into integer

---
+++ integer/integer.js
@@ -0,0 +1,5 @@
+let dividend = 10;
+let divisor = 3;
+let reminder = dividend % divisor;
+let integer = (dividend - reminder) / divisor;
+console.log("Integer value of ", dividend, "/", divisor, "is", integer);
